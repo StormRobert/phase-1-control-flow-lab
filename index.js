0d6c0459abde87175884998406499ebf5385d431
storm <stormarasa2@gmail.com>
2023-09-18 20:46:34 +0300

    JS.3 assignment done

--- index.js
+++ index.js
@@ -1,11 +1,44 @@
-function scuberGreetingForFeet(){
+function scuberGreetingForFeet(x) {
   // Write your code here!
+  if (x <= 400) {
+    return 'This one is on me!'
+  } else if (x > 2000 && x <= 2500) {
+    return 'I will gladly take your thirty bucks.'
+  } else {
+    return 'No can do.'
+  }
 }
 
-function ternaryCheckCity(){
+//const y = 'NYC';
+//function ternaryCheckCity(y) {
   // Write your code here!
-}
 
-function switchOnCharmFromTip(){
+ // if (   y === 'NYC ') {
+ //   return 'Ok, sounds good.'
+
+  //} else {
+ //   return 'No go.'
+  //}
+//}
+
+const y = 'NYC';
+ function ternaryCheckCity(y) {
+ return (y === 'NYC') ? `Ok, sounds good.` : `No go.`;
+ }
+
+
+const z = 'generous';
+function switchOnCharmFromTip(z) {
   // Write your code here!
+  switch (z){
+    case 'generous':
+      return ('Thank you so much.');
+
+      case 'not as generous':
+        return('Thank you.');
+
+        default:
+          return ('Bye.');
+  }
+
 }
